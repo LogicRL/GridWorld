5d4a5ef15c9162dc2e08c60e5662efd1117a64cd
David Qiu <david@davidqiu.com>
2018-05-15 21:30:05 -0400

    added Q-learning agent and single agent script

---
+++ src/QLearningAgent.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+
+"""
+QLearningAgent.py
+Q-learning agent for discrete environment with dynamic state and transition 
+learning. The environment shall be stationary in terms of state transition and 
+reward mechanism.
+"""
+
+__version__     = "0.0.1"
+__author__      = "David Qiu"
+__email__       = "dq@cs.cmu.edu"
+__website__     = "http://www.davidqiu.com/"
+__copyright__   = "Copyright (C) 2018, David Qiu. All rights reserved."
+
+
+import numpy as np
+import gym
+import time
+
+import pdb, IPython
+
+
+class QLearningAgent(object):
+  """
+  Q-learning agent class.
+  """
+
+  def __init__(self, n_action):
+    """
+    Initialize a Q-learning agent.
+
+    @param n_action The number of possible actions to take.
+    """
+
+    super(QLearningAgent, self).__init__()
+
+    self.n_action = n_action
+
+    #self.P = dict() # state transition model - {s: [{s_next: count}, ...]}
+    #self.R = dict() # reward model - {s: (r_ave, count)}
+    self.Q = dict() # state-action value model - {s: [q, ...]}
+
+
+  def act(self, s, epsilon=0):
+    """
+    Predict the next action at a state.
+
+    @param s The state the agent is at.
+    @param epsilon The exploration randomness.
+    """
+
+    a_random = int(np.floor(np.random.random() * 4))
+
+    if s not in self.Q:
+      self.Q[s] = [0] * self.n_action
+
+    a = np.argmax(self.Q[s])
+
+    if np.random.random() < epsilon:
+      a = a_random
+
+    return a
+
+
+  def learn(self, s, a, s_next, r, lr=0.01, gamma=1.0):
+    """
+    Learn new experience.
+
+    @param s The original state.
+    @param a The action taken.
+    @param s_next The state transitted to.
+    @param r The reward received.
+    @param lr The learning rate.
+    @param gamma The reward decay rate.
+    """
+
+    if s not in self.Q:
+      self.Q[s] = [0] * self.n_action
+
+    if s_next not in self.Q:
+      self.Q[s_next] = [0] * self.n_action
+
+    Q_update = r + gamma * np.max(self.Q[s_next])
+
+    self.Q[s][a] += lr * (Q_update - self.Q[s][a])
+
---
+++ src/launch_single.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python
+
+"""
+launch_single.py
+Launch the Grid Minecraft game with single agent.
+"""
+
+__version__     = "0.0.1"
+__author__      = "David Qiu"
+__email__       = "dq@cs.cmu.edu"
+__website__     = "http://www.davidqiu.com/"
+__copyright__   = "Copyright (C) 2018, David Qiu. All rights reserved."
+
+
+import numpy as np
+import gym
+import time
+from GridMinecraftEnv import GridMinecraftEnv
+from QLearningAgent import QLearningAgent
+from MBAgent import MBAgent
+
+import pdb, IPython
+
+
+def print_state_action_values(Q):
+  """
+  Print state-action values.
+  """
+
+  for s in Q:
+    print(s)
+    print(Q[s])
+    print('')
+
+
+def run_episode(env, agent, epsilon=0, lr=0.01, gamma=1.0, learn=True, render=False, verbose=False):
+  """
+  Run the agent for an episode in the environment.
+
+  @param env The environment.
+  @param agent The agent to run.
+  @param epsilon The exploration randomness.
+  @param lr The learning rate.
+  @param gamma The reward decay rate.
+  @param learn The switch to turn on learning.
+  @param render The switch to turn on rendering.
+  @param verbose The switch to turn on verbose logging.
+  """
+
+  acc_rewards = 0
+
+  s = env.reset()
+  if render:
+    env.render()
+    time.sleep(0.5)
+
+  done = False
+  while not done:
+    # choose an action
+    a = agent.act(s, epsilon=epsilon)
+
+    # environment roll forward
+    s_next, r, done, info = env.step(a)
+    acc_rewards += r
+
+    # learn experience
+    if learn:
+      agent.learn(s, a, s_next, r, lr=lr, gamma=gamma)
+    
+    # update state
+    s = s_next
+
+    if verbose:
+      print('r = %f' % (r, acc_rewards))
+      print('')
+
+    if render:
+      env.render()
+      time.sleep(0.1)
+
+  return acc_rewards
+
+
+def main():
+  """
+  Program entry.
+  """
+
+  # initialize the environment
+  env = GridMinecraftEnv()
+
+  # initialize agent
+  agent = QLearningAgent(env.action_space.n)
+  #agent = MBAgent(env.action_space.n)
+
+  # run episodes
+  episodes = 1000
+  epsilon_max = 1.0
+  epsilon_decay = 1e-3
+  epsilon_min = 0.01
+  lst_return = []
+  for episode in range(episodes):
+    epsilon = max(epsilon_max - epsilon_decay * episode, epsilon_min)
+    
+    render = False
+    if episode == episodes - 1:
+      render = True
+
+    acc_rewards = run_episode(env, agent, 
+      epsilon=epsilon, lr=0.01, gamma=0.99, 
+      learn=True, render=render, verbose=False)
+    lst_return.append(acc_rewards)
+
+    print('episode: %d/%d, acc_rewards: %f, epsilon: %f' % (episode+1, episodes, acc_rewards, epsilon))
+
+    if episode == episodes - 1:
+      IPython.embed()
+    
+  print('average return: %f' % (np.mean(lst_return)))
+
+
+if __name__ == '__main__':
+  main()
